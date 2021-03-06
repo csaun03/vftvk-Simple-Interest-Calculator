5cdb34ded6d6c31ace4cf52b77a506a1b9ede3ab
csaun03 <cheri.saunders03@gmail.com>
2021-03-05 22:43:25 -0500

    calculator code

--- script.js
+++ script.js
@@ -1,6 +1,68 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+
+//range slider function
+
+function rangeSlider (){
+
+   let rangeNum = document.getElementById("rate").value;
+
+   document.getElementById("showRate").innerHTML = rangeNum + "%";
+
 }
+
+ 
+
+//results
+
+ 
+
+
+
+function compute()
+
+{
+
+    //variables
+
+const principal = document.getElementById("principal").value;
+
+const rate = document.getElementById("rate").value;
+
+const years = document.getElementById("years").value;
+
+const interest = principal * years * rate/100
+
+ 
+
+//logic for future year
+
+const calcYears = document.getElementById("years");
+
+const yearSelected = calcYears.value;
+
+const futureYear = parseInt(yearSelected)  + 2021;
+
+//console.log(futureYear);
+
+ //innerhtml
+    
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br>" + " at an interest rate of <mark>" + rate + "%</mark>.<br> You will receive an amount of <mark>" +  interest + "</mark>,<br> in the year <mark>" + futureYear + "</mark>";
+
+   //principal validation
+
+   if (principal <= 0){
+       
+        window.alert("Enter a positive number");
+        document.getElementById("principal").focus();
         
+   }
+
+
+
+
+   
+
+}
+
+    
+ 
